5d0a5334f5bad7dacfcd84a537e10b8bde4c11a8
Giftea <eleojogiftuhiene@gmail.com>
2022-11-27 01:26:35 +0100

    updated hardhat.config

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -2,5 +2,44 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+	solidity: {
+		compilers: [
+			{
+				version: "0.8.17",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 200,
+					},
+				},
+			},
+			{
+				version: "0.8.9",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 200,
+					},
+				},
+			},
+			{
+				version: "0.8.4",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 200,
+					},
+				},
+			},
+			{
+				version: "0.6.7",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 200,
+					},
+				},
+			},
+		],
+	},
 };
