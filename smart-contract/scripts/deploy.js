67fd07e928e4bc9701010be723418dc47188747c
Giftea <eleojogiftuhiene@gmail.com>
2022-11-29 22:12:13 +0100

    deploy script

--- smart-contract/scripts/deploy.js
+++ smart-contract/scripts/deploy.js
@@ -4,28 +4,94 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
+const { ethers, upgrades } = require("hardhat");
 const hre = require("hardhat");
+require("dotenv").config({ path: ".env" });
+
+const env = process.env.ENV ?? "testnet";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+	const [owner] = await ethers.getSigners();
+	let usdcAddress;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+	if (env === "testnet") {
+		const USDC = await hre.ethers.getContractFactory("USDC");
+		const usdc = await USDC.deploy();
+		await usdc.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+		usdcAddress = usdc.address; //mock usdc polygon testnet we control
+	} else {
+		usdcAddress = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174"; //usdc polygon mainnet (PoS)
+	}
 
-  await lock.deployed();
+	const Treasury = await hre.ethers.getContractFactory(
+		"TreasuryImplementation"
+	);
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+	const treasury = await upgrades.deployProxy(Treasury, [owner.address]);
+	await treasury.deployed();
+
+	const SAVEHToken = await hre.ethers.getContractFactory("SAVEHToken");
+	const saveh = await SAVEHToken.deploy(owner.address);
+	await saveh.deployed();
+
+	const SSAVEHToken = await hre.ethers.getContractFactory("SSAVEHToken");
+	const ssaveh = await SSAVEHToken.deploy();
+	await ssaveh.deployed();
+
+	const firstRewardPerBlock = hre.ethers.utils.parseEther("250");
+	const rewardPeriodSize = 17280; // 5 secs per block, 17280 blocks for 24 hours (mainnet = 43200)
+	// const startingBlock = 10611000;
+	const startingBlock = (await ethers.provider.getBlockNumber()) + 1000;
+	const decayNumerator = 998902;
+	const decayDenominator = 1000000;
+
+	const DonationMiner = await hre.ethers.getContractFactory(
+		"DonationMinerImplementation"
+	);
+	const donationMiner = await upgrades.deployProxy(DonationMiner, [
+		usdcAddress,
+		saveh.address,
+		treasury.address,
+		firstRewardPerBlock,
+		rewardPeriodSize,
+		startingBlock,
+		decayNumerator,
+		decayDenominator,
+	]);
+
+	await donationMiner.deployed();
+
+	const Staking = await hre.ethers.getContractFactory(
+		"StakingImplementation"
+	);
+	const staking = await upgrades.deployProxy(Staking, [
+		saveh.address,
+		ssaveh.address,
+		donationMiner.address,
+		259200, //cooldown
+	]);
+	await staking.deployed();
+
+	await donationMiner.updateStaking(staking.address);
+
+	console.log({
+		Enviroment: env.toLocaleUpperCase(),
+		USDC: usdcAddress,
+		"Donation Miner Proxy": donationMiner.address,
+		"Donation Miner Implementation":
+			await upgrades.erc1967.getImplementationAddress(
+				donationMiner.address
+			),
+		Treasury: treasury.address,
+		"SAVEH Token": saveh.address,
+		"SSAVEH Token": ssaveh.address,
+	});
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+	console.error(error);
+	process.exitCode = 1;
 });
