3f7e4ade9623424b6545a1f1d66b0ae55002fab3
Giftea <eleojogiftuhiene@gmail.com>
2022-11-30 03:06:14 +0100

    deploy contract

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -1,7 +1,51 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@openzeppelin/hardhat-upgrades");
+require("dotenv").config({ path: ".env" });
+require("@nomiclabs/hardhat-etherscan");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+	etherscan: {
+		// Your API key for Etherscan
+		// Obtain one at https://etherscan.io/
+		apiKey: process.env.POLYGON_API_KEY,
+	},
+	networks: {
+		hardhat: {
+			allowUnlimitedContractSize: true,
+			timeout: 100000,
+			gasPrice: "auto",
+			gas: 13000000,
+		},
+		localhost: {
+			url: "http://127.0.0.1:8545",
+			allowUnlimitedContractSize: true,
+			accounts: [process.env.PRIVATE_KEY],
+			timeout: 100000,
+			gasPrice: "auto",
+			gas: 13000000,
+		},
+		mumbai: {
+			chainId: 80001,
+			url: "https://matic-mumbai.chainstacklabs.com",
+			hardfork: "istanbul",
+			accounts: [process.env.PRIVATE_KEY],
+			allowUnlimitedContractSize: true,
+			gas: "auto",
+			gasPrice: "auto",
+			blockGasLimit: 13000000,
+		},
+		mainnet: {
+			chainId: 137,
+			url: "https://polygon-rpc.com",
+			hardfork: "istanbul",
+			accounts: [process.env.PRIVATE_KEY],
+			allowUnlimitedContractSize: true,
+			gas: "auto",
+			gasPrice: "auto",
+			blockGasLimit: 13000000,
+		},
+	},
 	solidity: {
 		compilers: [
 			{
